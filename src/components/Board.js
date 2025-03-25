660728bbff1f6cc1fd7b667a54b22952d6b1927c
minh-lgm <minh.lgm@gmail.com>
2025-03-25 16:11:06 +0700

    upload

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,9 +5,21 @@ export default function Board({ squares, handleClick }) {
   return (
     <div className="board">
       <div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
+        <div className="board-row">
+          {Array.from({ length: 3 }).map((_, i) => (
+            <Square key={i} value={squares[i]} onClick={() => handleClick(i)} />
+          ))}
+        </div>
+        <div className="board-row">
+          {Array.from({ length: 3 }).map((_, i) => (
+            <Square key={i + 3} value={squares[i + 3]} onClick={() => handleClick(i + 3)} />
+          ))}
+        </div>
+        <div className="board-row">
+          {Array.from({ length: 3 }).map((_, i) => (
+            <Square key={i + 6} value={squares[i + 6]} onClick={() => handleClick(i + 6)} />
+          ))}
+        </div>
       </div>
     </div>
   );
